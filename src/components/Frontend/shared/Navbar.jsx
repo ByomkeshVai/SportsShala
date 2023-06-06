817e23ed4bb41717626bf16c39f5ad3e48dc4d2b
byomkesh2580 <habibrahman2580@gmail.com>
2023-06-06 21:18:41 +0600

    NavbarFixed

--- src/components/Frontend/shared/Navbar.jsx
+++ src/components/Frontend/shared/Navbar.jsx
@@ -4,16 +4,17 @@ import { Link } from 'react-router-dom';
 import logo from '../../../assets/images/logo.png'
 
 const Navbar = () => {
-    const { user, logOut } = useContext(AuthContext);
-
-    const handleLogOut = () => {
+   const { user, logOut } = useContext(AuthContext)
+  
+  const handleLogOut = () => {
         logOut()
             .then()
             .catch(error => console.log(error));
     }
+    
     return (
         <>
-            <div className="navbar bg-base-100">
+            <div className="navbar">
   <div className="navbar-start">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -21,29 +22,19 @@ const Navbar = () => {
       </label>
       <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/alltoys">All Toys</Link></li>
-            {
-                  user && <>
-                  <li><Link to="/mytoys">My Toys</Link></li>
-                    <li><Link to="/addtoys">Add A Toy</Link></li>
-                  </>
-            }
-        <li><Link to="/blogs">Blogs</Link></li>
+        <li><Link to="/instructor">Instructor</Link></li>
+        <li><Link to="/classes">Classes</Link></li>
+        <li><Link to="/dashboard">Dashboard</Link></li>
       </ul>
     </div>
     <a className="btn btn-ghost normal-case text-xl"><img src={logo} alt="" className='lg:h-16 lg:w-16 h-6 mr-2'/> <Link to="/">PowerToy</Link></a>
   </div>
   <div className="navbar-end hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-       <li><Link to="/">Home</Link></li>
-        <li><Link to="/alltoys">All Toys</Link></li>
-         {
-                  user && <>
-                  <li><Link to="/mytoys">My Toys</Link></li>
-                    <li><Link to="/addtoys">Add A Toy</Link></li>
-                  </>
-            }
-        <li><Link to="/blogs">Blogs</Link></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/instructor">Instructor</Link></li>
+        <li><Link to="/classes">Classes</Link></li>
+        <li><Link to="/dashboard">Dashboard</Link></li>
     </ul>
                 </div>
   <div className="">
