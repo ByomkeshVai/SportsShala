84a9e1a1ceb73c341832335d5ed3593d7595810d
byomkesh2580 <habibrahman2580@gmail.com>
2023-06-06 21:11:23 +0600

    Navbar

---
+++ src/components/Frontend/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './shared/Navbar';
+
+const Home = () => {
+    return (
+        <div className='my-container'>
+            <Navbar></Navbar>
+            
+        </div>
+    );
+};
+
+export default Home;
