a86df673ac3f649c50b929cc6c17e5d2ebbbfa78
byomkesh2580 <habibrahman2580@gmail.com>
2023-06-08 04:57:23 +0600

    SwuperAutoplay

--- src/components/Frontend/FrontSlider/FrontSlider.jsx
+++ src/components/Frontend/FrontSlider/FrontSlider.jsx
@@ -6,8 +6,9 @@ import { useQuery } from '@tanstack/react-query';
 import { useContext } from 'react';
 import { AuthContext } from '../../../providers/AuthProvider';
 import "swiper/css";
-import "swiper/css/pagination";
 import Header from '../shared/Header';
+import 'swiper/css/autoplay';
+import { Autoplay } from 'swiper';
 
 const FrontSlider = () => {
 
@@ -24,31 +25,31 @@ const FrontSlider = () => {
     },
   })
     
-    console.log(sliderClass);
+    
+
+
 
     return (
-        <div className='mt-8 max-w-screen-xl mx-auto'>
-                <Header slogan={"Learn Unlimited Sports Lessions With Sports Shala"}></Header>
+        <div className='mt-8 max-w-screen-xl mx-auto '>
+
+                <Header slogan={"Learn Unlimited Popular Sports Lessions"}></Header>
             
-             <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        pagination={{
-            clickable: true,
-            autoplay: {delay: 3000},
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
+            <div className="border border-2 shadow-xl p-3 bg-gradient-to-l from-sky-800 to-indigo-900 p-8 rounded-xl shadow-xl">
+                <Swiper
+                    modules={[Autoplay]}
+        slidesPerView={4}
+         autoplay={{ delay: 1000 }}
+        className="mySwiper swiper-slide"
         >
                 {
                     sliderClass?.slice(0, 6).map(sliderClass => (
                         <SwiperSlide>
-                        <div className="card w-80 bg-base-100 shadow-xl">
-                        <figure className="px-6 pt-10">
-                            <img src={sliderClass?.image} alt="Shoes" className="mx-auto object-cover rounded h-56 w-64 rounded-xl" />
+                        <div className="card w-64 bg-base-100 shadow-xl ml-6 hover:scale-110">
+                        <figure className="px-2 pt-3">
+                            <img src={sliderClass?.image} alt="Shoes" className="mx-auto object-cover h-56 w-64 rounded-xl  " />
                         </figure>
                         <div className="card-body items-center text-center">
-                            <h2 className="card-title">{sliderClass.name}</h2>
+                            <h2 className="card-title mt-[-24px]">{sliderClass.name}</h2>
                             <p>{sliderClass.instructor.name}</p>
                             <div className="card-actions">
                             <button className="btn btn-sm btn-primary mt-3">Learn More</button>
@@ -61,6 +62,7 @@ const FrontSlider = () => {
          }
                
       </Swiper>
+           </div>
         </div>
     );
 };
