c033bd10d18105e08fa41576cdb8e9089aaa121b
byomkesh2580 <habibrahman2580@gmail.com>
2023-06-08 15:43:58 +0600

    selectbutton

--- src/components/Frontend/classArea/ClassCard.jsx
+++ src/components/Frontend/classArea/ClassCard.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { toast } from 'react-hot-toast';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../providers/AuthProvider';
+import useSelect from '../../../hooks/useSelect';
 
-const ClassCard = ({allClass}) => {
+const ClassCard = ({ allClass }) => {
+    
+    const { image, name, price, seats, _id } = allClass;
+    
+    const { user } = useContext(AuthContext);
+    const [refetch] = useSelect();
+    const navigate = useNavigate();
+    const location = useLocation();
+
+     const handleAddToSelect = allClass => {
+        console.log(allClass);
+        if(user && user.email){
+            const selectItem = {selectItemId: _id, name, image, price, email: user.email, instructor: allClass.instructor.email }
+            fetch(`${import.meta.env.VITE_API_URL}/select`, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                    authorization: `Bearer ${localStorage.getItem('access-token')}`,
+                },
+                body: JSON.stringify(selectItem)
+            })
+            .then(res => res.json())
+            .then(data => {
+                if(data.insertedId){
+                    refetch();
+                   toast("Class Selected, Check Dashboard")
+                }
+            })
+        }
+        else{
+            navigate('/login', {state: {from: location}})
+
+        }
+    }
+
+
+
     return (
-         <Link to={`/class/${allClass?._id}`} className='col-span-2 cursor-pointer group'>
+         <div className='col-span-2 cursor-pointer group'>
       <div className='flex flex-col gap-2 w-full border border-2 shadow-xl rounded-xl p-6'>
         <div
           className='
@@ -22,17 +62,17 @@ const ClassCard = ({allClass}) => {
               group-hover:scale-110 
               transition
             '
-            src={allClass.image}
+            src={image}
             alt='Class'
           />
         </div>
-         <div className='font-semibold text-lg text-center '>{allClass.name}</div>
+         <div className='font-semibold text-lg text-center '>{name}</div>
                 <p className="text-gray-700 text-base mx-auto">Instructor: {allClass.instructor.name}</p>
-        <p className="text-gray-700 text-base mx-auto">Available Seats: {allClass.seats}</p>
-                <p className="text-gray-700 text-base mx-auto">Price: {allClass.price}</p>
-                <button className="btn btn-sm btn-info mt-3">Learn More</button>
+        <p className="text-gray-700 text-base mx-auto">Available Seats: {seats}</p>
+                <p className="text-gray-700 text-base mx-auto">Price: {price}</p>
+                <button className="btn btn-sm btn-info mt-3" onClick={() => handleAddToSelect(allClass)}>Select Class</button>
       </div>
-    </Link>
+    </div>
     );
 };
 
