6f94ebd9e667266f0dc48681aa33b9c946d25a93
byomkesh2580 <habibrahman2580@gmail.com>
2023-06-10 06:09:24 +0600

    sliderBreakPoint

--- src/components/Frontend/FrontSlider/FrontSlider.jsx
+++ src/components/Frontend/FrontSlider/FrontSlider.jsx
@@ -36,17 +36,26 @@ const FrontSlider = () => {
             
             <div className="border border-2 shadow-xl p-3 bg-gradient-to-l from-sky-800 to-indigo-900 p-8 rounded-xl shadow-xl">
                 <Swiper
+                    breakpoints={{
+        320: { slidesPerView: 1, spaceBetween: 80 },
+        480: { slidesPerView: 1, spaceBetween: 150 },
+        768: { slidesPerView: 2, spaceBetween: 50 },
+        1024: { slidesPerView: 4, spaceBetween: 150 },
+      }}
                     modules={[Autoplay]}
         slidesPerView={4}
                     autoplay={{
                         delay: 1000,
-        disableOnInteraction: true            }}
+                        disableOnInteraction: true
+    
+    
+                    }}
         className="mySwiper swiper-slide"
         >
                 {
                     sliderClass?.slice(0, 6).map(sliderClass => (
                         <SwiperSlide>
-                        <div className="card w-64 bg-base-100 shadow-xl ml-6 hover:scale-110">
+                        <div className="card lg:w-64 w-full bg-base-100 shadow-xl lg:ml-6 hover:scale-110">
                         <figure className="px-2 pt-3">
                             <img src={sliderClass?.image} alt="Shoes" className="mx-auto object-cover h-56 w-64 rounded-xl  " />
                         </figure>
--- src/components/Frontend/shared/Header.jsx
+++ src/components/Frontend/shared/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Header = ({slogan}) => {
     return (
-         <div class="container flex justify-center mx-auto ">
+         <div class="container flex justify-center mx-auto p-6">
                      <div className="gallery-head mx-auto mt-8">
-                <h2 className='text-3xl font-bold '>{slogan}</h2>
+                <h2 className='text-3xl font-bold text-center'>{slogan}</h2>
                 <div className="divider before:bg-gray-900 after:bg-gray-900 mb-10"></div>
                 </div>
                 </div>
