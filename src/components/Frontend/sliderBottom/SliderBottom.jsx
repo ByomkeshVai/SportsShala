163f28ecbadc7bf01dc4e7349584e983dcc9c3c7
byomkesh2580 <habibrahman2580@gmail.com>
2023-06-10 06:01:46 +0600

    sliderrbreadcumbresponsive

--- src/components/Frontend/sliderBottom/SliderBottom.jsx
+++ src/components/Frontend/sliderBottom/SliderBottom.jsx
@@ -8,51 +8,51 @@ import { AiOutlineFileProtect } from 'react-icons/Ai';
 
 const SliderBottom = () => {
     return (
-         <div className='max-w-screen-xl mx-auto rounded-xl mt-[-70px] absolute bottom-0 left-0 bg-slate-50 relative center mx-auto'>
-            <div className="main p-8 rounded-xl lg:flex gap-6 shadow-lg shadow-gray-500/50  justify-items-center items-center">
-                 <div className="text1 mx-auto" >
-                    <div className="student flex items-center gap-4">
-                        <div className="logo">
+         <div className='max-w-screen-xl lg:mx-auto text-center lg:text-left rounded-xl lg:mt-[-70px] lg:absolute lg:bottom-0 lg:left-0 bg-slate-50 lg:relative center mx-auto'>
+            <div className="main lg:p-8 p-4 rounded-xl lg:flex lg:flex-row flex flex-row lg:gap-6 shadow-lg shadow-gray-500/50 justify-items-center ;lg:items-center">
+                 <div className="text1 lg:mx-auto text-center lg:text-left" >
+                    <div className="student lg:flex ;lg:items-center lg:gap-4">
+                        <div className="logo invisible lg:visible mb-[-38px] lg:mb-0 ">
                             <GrUserSettings size={52}></GrUserSettings>
                         </div>
                         <div className="text-area">
-                        <h3 className='text-4xl mb-3'>25K+</h3>
-                    <p className='text-xl'>Students</p>
+                        <h3 className='lg:text-4xl text-2xl lg:mb-3'>25K+</h3>
+                    <p className='lg:text-xl text-lg'>Students</p>
                         </div>
                    </div>
                 </div>
-                <div className="text2  mx-auto">
-                    <div className="student flex items-center gap-4">
-                        <div className="logo">
+                <div className="text2  lg:mx-auto text-center lg:text-left">
+                    <div className="student lg:flex ;lg:items-center lg:gap-4">
+                        <div className="logo invisible lg:visible mb-[-38px] lg:mb-0">
                             <MdSportsCricket size={52}></MdSportsCricket>
                         </div>
                         <div className="text-area">
-                            <h3 className='text-4xl mb-3'>100+</h3>
-                            <p className='text-xl'>Experts Sports Man</p>
+                            <h3 className='lg:text-4xl text-2xl lg:mb-3'>100+</h3>
+                            <p className='lg:text-xl text-lg'>Experts Sports Man</p>
                         </div>
                    </div>
                     
                 </div>
-                <div className="text3 mx-auto">
-                    <div className="student flex items-center gap-4">
-                        <div className="logo">
+                <div className="text3 lg:mx-auto text-center lg:text-left">
+                    <div className="student lg:flex ;lg:items-center lg:gap-4">
+                        <div className="logo invisible lg:visible mb-[-38px] lg:mb-0">
                             <SiGithubsponsors size={52}></SiGithubsponsors>
                         </div>
                         <div className="text-area">
-                       <h3 className='text-4xl mb-3'>5k+</h3>
-                    <p className='text-xl'>Sponsors</p>
+                       <h3 className='lg:text-4xl text-2xl lg:mb-3'>5k+</h3>
+                    <p className='lg:text-xl text-lg'>Sponsors</p>
                         </div>
                    </div>
                    
                 </div>
-                <div className="text4 mx-auto">
-                    <div className="student flex items-center gap-4">
-                        <div className="logo">
+                <div className="text4 lg:mx-auto text-center lg:text-left">
+                    <div className="student lg:flex ;lg:items-center lg:gap-4">
+                        <div className="logo invisible lg:visible mb-[-38px] lg:mb-0">
                             <AiOutlineFileProtect size={52}></AiOutlineFileProtect>
                         </div>
                         <div className="text-area">
-                        <h3 className='text-4xl mb-3'>500</h3>
-                            <p className='text-xl'>Professional Player</p>
+                        <h3 className='lg:text-4xl text-2xl lg:mb-3'>500</h3>
+                            <p className='lg:text-xl text-lg'>Professional Player</p>
                         </div>
                    </div>
                     
